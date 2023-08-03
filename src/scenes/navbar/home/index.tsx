9a620a9cd1d7e339f9fc8999b9d963027016ca0b
Jacob Ehrler <jacobehrler@ShaniquesiPad2.localdomain>
2023-08-03 16:08:16 -0400

    Responsive Navbar created.

---
+++ src/scenes/navbar/home/index.tsx
@@ -0,0 +1,25 @@
+// import { SelectedPage } from "@/shared/types";
+// import useMediaQuery from "@/hooks/useMediaQuery";
+// import ActionButton from "@/shared/ActionButton";
+// import HomePageText from "@/assets/"
+
+// type Props = {
+//     setSelectedPage: (value: SelectedPage) => void;
+// }
+
+// const Home = ({setSelectedPage}: Props) => {
+//     const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
+//   return (
+//     <section
+//     id="home"
+//     className="gap-16 bg-gray-20 pb-10 pt-10 md:h-full md:pb-0"
+//     >
+//         {/* {image and main header } */}
+//         <div>
+
+//         </div>
+//     </section>
+//   )
+// };
+
+// export default Home;
