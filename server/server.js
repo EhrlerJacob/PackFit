cb649a9c89d38e86132beec5b9f2380df571be06
Jacob Ehrler <jacobehrler@Jacobs-MBP.lan>
2023-08-15 13:24:39 -0400

    server running full crud

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+require('./config/mongoose.config')
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+const BlogRoutes = require('./routes/blogRoutes');
+BlogRoutes(app);
 
 app.listen(8000, () => {
     console.log("Server Running on Port 8000")
